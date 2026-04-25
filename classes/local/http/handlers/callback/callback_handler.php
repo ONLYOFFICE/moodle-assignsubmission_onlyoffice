056e6306f3b657a6a3ef1ce8329d219d643835d7
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2026-04-25 18:59:37 +0500

    fix: handle STATUS_CLOSEDNOCHANGES as no-op instead of triggering save

--- classes/local/http/handlers/callback/callback_handler.php
+++ classes/local/http/handlers/callback/callback_handler.php
@@ -53,10 +53,10 @@ abstract class callback_handler {
         switch ($this->request->callbackdata->status) {
             case util::STATUS_MUSTSAVE:
             case util::STATUS_ERRORSAVING:
-            case util::STATUS_CLOSEDNOCHANGES:
                 $result = $this->handle_save();
                 break;
 
+            case util::STATUS_CLOSEDNOCHANGES:
             case util::STATUS_EDITING:
                 $result = self::RESULT_OK;
                 break;
