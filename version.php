01510682b082aa5eacf72f3421dd07428c4caf80
Sergey Linnik <sergey.linnik@onlyoffice.com>
2026-05-08 12:52:07 +0300

    require assignsubmission_onlyoffice v6.2.0

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->version = 2025080401;
 $plugin->release = 'v4.1.0';
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = ['mod_onlyofficeeditor' => 2024111300];
+$plugin->dependencies = ['mod_onlyofficeeditor' => 2026050800];
